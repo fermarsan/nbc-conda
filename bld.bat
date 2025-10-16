7dc0a3cfc1c52f93a5b28c979ce38758e4dfddd5
fermarsan <fmartinezsanta@gmail.com>
2025-10-15 21:43:38 -0500

    Initial commit

---
+++ bld.bat
@@ -0,0 +1,5 @@
+@echo off
+mkdir %PREFIX%\bin
+copy %SRC_DIR%\win-64\bin\nbc.exe %PREFIX%\bin\nbc.exe
+
+
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+cp $SRC_DIR/linux-64/bin/nbc $PREFIX/bin
